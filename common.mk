233b7fa7e328e621798d3226363f4f9d83445b06
mmj <chenzhangmeng1999@gmail.com>
2026-09-05 18:28:27 +0800

    build: isolate generated parser warnings

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
-AM_CXXFLAGS = -g -Wall -Wpointer-arith -Wwrite-strings -Wno-unused -Wno-overloaded-virtual
+MATIEC_HANDWRITTEN_CXXFLAGS = -g -Wall -Wpointer-arith -Wwrite-strings -Wno-unused -Wno-overloaded-virtual
+AM_CXXFLAGS = $(MATIEC_HANDWRITTEN_CXXFLAGS)
 ARFLAGS = cr
-
 
